6a061c2ff03be52961832391d4c0c107dfee9a7d
Shlomi <shlomivaknin@gmail.com>
2012-09-16 16:28:05 +0300

    more progress on the protocol

--- src/core/Xushi.scala
+++ src/core/Xushi.scala
@@ -6,22 +6,32 @@ import scala.util.parsing.combinator.syntactical.StandardTokenParsers
 import scala.collection.immutable.TreeMap
 
 class Xushi {
-  var store = new HashMap[String, Any]
+  var store = new TreeMap[String, Option[Any]]
   var peers = new HashMap[StoreConnection, List[String]]
 
+  def diceKey(key:String)=key.split("\\.").inits.filterNot(_.isEmpty).map((x) => x reduce (_.concat(".") ++ _)).toStream.reverse
+  
   def interested(key: String) = {
-    val diced = key.split("\\.").inits.filterNot(_.isEmpty).map((x) => x reduce (_.concat(".") ++ _)).toStream.reverse
+    val diced = diceKey(key)
     peers.filter((p) => diced.exists(p._2.contains(_))).map(_._1)
   }
 
   def add(key: String, v: Any) = {
-    store + ((key, v))
-    interested(key) foreach (send(key, v)(_))
+    diceKey(key).foreach((mk)=> store.get(mk).getOrElse{store = store + ((mk, None))})
+    store = store + ((key, Some(v)))
+    interested(key) foreach (send(_)(key, v))
   }
 
-  val send = (key: String, v: Any) => (to: StoreConnection) => {
-    println("sending to " + to.getID() + " " + key + " " + v)
-    to.sendTCP(new ADD(key, v))
+  val send = (to: StoreConnection)=>(x:(String, Any))=> x match {
+    case (key, v) => {
+    	println("sending to " + to.getID() + " " + key + " " + v)
+    	to.sendTCP(new ADD(key, v))
+    }
+  } 
+    
+  val getAllItemsFromStore = (key:String)=>{  
+	  val head = key.split("\\.",1)(0)
+	  store.from(head).takeWhile(_._1.startsWith(head)).filter(_._2.isDefined).map {case (k, Some(v))=>(k,v)}
   }
 
   val server = new StoreServer(9999)
@@ -42,7 +52,10 @@ class Xushi {
         obj match {
           case c: CMD => c match {
             case ADD(key, v) => add(key, v)
-            case LISTEN(key) => peers = peers + ((conn, peers.get(conn).getOrElse(List()).::(key)))
+            case LISTEN(key) => {
+              peers = peers + ((conn, peers.get(conn).getOrElse(List()).::(key)))
+              getAllItemsFromStore(key).foreach(send(conn))          
+              }
           }
           case _=>
         }
@@ -58,10 +71,10 @@ class Xushi {
 
 object Xushi {
   def main(args: Array[String]) {
-    var t = new TreeMap[String, Int]
-    t = t + ("a" -> 1) + ("b"->2) + ("a.1"->3) + ("a.2"->4) + ("a.2.t"->5)+("b.1"->6)
-    t.takeWhile(_._1.startsWith("b")).foreach(println _)
-    val a = new Xushi;
+ //   var t = new TreeMap[String, Int]
+ //   t = t + ("a" -> 1) + ("b"->2) + ("a.1"->3) + ("a.2"->4) + ("a.2.t"->5)+("b.1"->6)
+ //   t.takeWhile(_._1.startsWith("b")).foreach(println _)
+ val a = new Xushi;
     // a.peers = a.peers + ("a" -> List("key1", "key2")) + ("b" -> List("key1.key6", "key2.key5"))
     //a.add("key1.key6.k1.2.34.5.6.778.fsfg.sd.er", 23)
     a.init
