3902c26c9eb4df4b716cdea3c447f235f2871b89
Shlomi <shlomivaknin@gmail.com>
2012-09-18 11:56:59 +0300

    moved peers info into StoreConnection

--- src/core/StoreServer.scala
+++ src/core/StoreServer.scala
@@ -24,12 +24,18 @@ abstract class Store[A](val port: Int) {
     }
 
     class LISTENSerializer extends Serializer[LISTEN] {
-      override def read(kryo: Kryo, input: Input, t: Class[LISTEN]): LISTEN = LISTEN(input.readString())
-      override def write(kryo: Kryo, output: Output, v: LISTEN) = output.writeString(v.key)
+      override def read(kryo: Kryo, input: Input, t: Class[LISTEN]): LISTEN = LISTEN(input.readString(), input.readString())
+      override def write(kryo: Kryo, output: Output, v: LISTEN) = {
+        output.writeString(v.key)
+        output.writeString(v.what)
+      }
     }
     class UNLISTENSerializer extends Serializer[UNLISTEN] {
-      override def read(kryo: Kryo, input: Input, t: Class[UNLISTEN]): UNLISTEN = UNLISTEN(input.readString())
-      override def write(kryo: Kryo, output: Output, v: UNLISTEN) = output.writeString(v.key)
+      override def read(kryo: Kryo, input: Input, t: Class[UNLISTEN]): UNLISTEN = UNLISTEN(input.readString(),input.readString())
+      override def write(kryo: Kryo, output: Output, v: UNLISTEN) = {
+    	  output.writeString(v.key)
+    	  output.writeString(v.what)
+      }
     }
     class DELSerializer extends Serializer[DEL] {
       override def read(kryo: Kryo, input: Input, t: Class[DEL]): DEL = DEL(input.readString())
@@ -53,32 +59,9 @@ abstract class Store[A](val port: Int) {
 abstract class CMD
 	case class ADD(key: String, v: Any) extends CMD
 	case class DEL(key: String) extends CMD
-	case class LISTEN(key: String) extends CMD
-	case class UNLISTEN(key:String) extends CMD
+	case class LISTEN(key: String, what:String) extends CMD
+	case class UNLISTEN(key:String,what:String) extends CMD
 
-class StoreConnection extends Connection
-
-class StoreServer(port: Int) extends Store(port) {
-  val server = new Server(16384, 2048, new KryoSerialization(kryi)) {
-    override def newConnection(): Connection = {
-      new StoreConnection
-    }
-  }
-
-  val accept = () => {
-    server.bind(port)
-    server.addListener(new Listener {
-      override def connected(c: Connection) {
-        println("new connection")
-      }
-      override def received(c: Connection, obj: Object) {
-        val conn = c.asInstanceOf[StoreConnection]
-      }
-    })
-
-    server.start()
-  }
-}
 
 class StoreClient(port: Int) extends Store(port) {
   val client = new Client(8192, 2048, new KryoSerialization(kryi))
@@ -113,18 +96,12 @@ object StoreServerTester {
       }
       override def disconnected(c: Connection) {
       }
-      override def received(c: Connection, obj: Object) {
-        obj match {
+      override def received(c: Connection, obj: Object) = obj match {
           case c: CMD => c match {
-            case ADD(key, v) => 
-              diceKey(key).foreach{ (k)=>
-	              val l = disptcher.get(k)
-	              l.foreach(_ foreach (_._2(key, v)))
-              }
+            case ADD(key, v) => diceKey(key).foreach{(k)=>disptcher.get(k).foreach(_ foreach (_._2(key, v)))}
           }
           case _ =>
         }
-      }
     })
 
     def diceKey(key:String)=key.split("\\.").inits.filterNot(_.isEmpty).map((x) => x reduce (_.concat(".") ++ _)).toStream.reverse
@@ -146,32 +123,38 @@ object StoreServerTester {
 */
    // Thread.sleep(3000)
     //onChange("bss.3") {(x,y)=>println ("1 " + x + " -> " + y)}
-    //onChange("bss.3.2") {(x,y)=>println ("2 "+x + " -> " + y)}
     val i = onChange("bss") {(x,y)=>println ("3 "+x + " -> " + y)}
+    onChange("bss") {(x,y)=>println ("2 "+x + " -> " + y)}
     Thread.sleep(3000)
-    client.client.sendTCP(new ADD("bss.1.2.3.3", 64))
-    client.client.sendTCP(new ADD("bss.2.2.3.3", 64))
-    client.client.sendTCP(new ADD("bss.3.2.3.3", 64))
+    client.client.sendTCP(new ADD("bss.1.2.3.3", 1))
+    client.client.sendTCP(new ADD("bss.2.2.3.3", 2))
+    client.client.sendTCP(new ADD("bss.3.2.3.3", 3))
+        Thread.sleep(3000)
+
     stop(i)
-    client.client.sendTCP(new ADD("bss.3.3.3.3", 64))
-    client.client.sendTCP(new ADD("bss.3.3.3.4", 64))
-    client.client.sendTCP(new ADD("bss.1.2.3.3", 64))
+    client.client.sendTCP(new ADD("bss.3.3.3.3", 4))
+    client.client.sendTCP(new ADD("bss.3.3.3.4", 5))
+    client.client.sendTCP(new ADD("bss.1.2.3.3", 6))
+    Thread.sleep(3000)
+    onChange("bss") {(x,y)=>println ("4 "+x + " -> " + y)}
     Thread.sleep(5000000)
   }
   
   val ai = new AtomicInteger(0)
   var disptcher = new HashMap[String, HashMap[String, (String, Any)=>Unit]]
   val onChange = (key : String) => (e:(String, Any)=>Unit) => {
-	val uniqueKey = key + "%" + ai.getAndIncrement()
-	disptcher = disptcher + ((key, disptcher.get(key).getOrElse(new HashMap) + ((uniqueKey, e))))
-	client.client.sendTCP(new LISTEN(key))
+	val uniqueKey = key + "%ADD%" + ai.getAndIncrement()
+	val l = disptcher.get(key).getOrElse(new HashMap)
+	disptcher = disptcher + ((key, l+ ((uniqueKey, e))))
+
+	client.client.sendTCP(new LISTEN(key, "ADD"))
     uniqueKey
   }
   val stop = (lid:String)=>{
-    val key = lid.split("%",2)(0);
-    disptcher = disptcher + ((key, disptcher.get(key).getOrElse(new HashMap) - lid))
-    println (disptcher)
-    client.client.sendTCP(new UNLISTEN(key))
+    val (key, what) = {val r = lid.split("%",3);(r(0),r(1))}
+    disptcher.get(key).map(_-lid).map((x)=>disptcher = disptcher + ((key, x)))
+    disptcher.get(key).foreach((x)=>if (x.isEmpty) disptcher = disptcher - key)
+    client.client.sendTCP(new UNLISTEN(key, what))
     Unit
   }
 }
--- src/core/Xushi.scala
+++ src/core/Xushi.scala
@@ -4,22 +4,29 @@ import com.esotericsoftware.kryonet.Listener
 import com.esotericsoftware.kryonet.Connection
 import scala.util.parsing.combinator.syntactical.StandardTokenParsers
 import scala.collection.immutable.TreeMap
+import scala.collection.immutable.Set
+import com.esotericsoftware.kryonet.Server
+import com.esotericsoftware.kryonet.KryoSerialization
+import scala.collection.immutable.TreeSet
 
 class Xushi {
   var store = new TreeMap[String, Option[Any]]
-  var peers = new HashMap[StoreConnection, List[String]]
+  //var peers = new HashMap[StoreConnection, Map[String, List[String]]]
+  var peers2 = List[StoreConnection]()
 
-  def diceKey(key:String)=key.split("\\.").inits.filterNot(_.isEmpty).map((x) => x reduce (_.concat(".") ++ _)).toStream.reverse
+  def diceKey(key:String)=
+    key.split("\\.").inits.filterNot(_.isEmpty).map((x) => x reduce (_.concat(".") ++ _)).toStream.reverse
   
-  def interested(key: String) = peers.filter((p) => diceKey(key).exists(p._2.contains(_))).map(_._1)
-
+  def interested(key: String, getList:(StoreConnection)=>List[String]) = 
+    peers2.filter((p)=>diceKey(key).exists(getList(p).contains(_)))
+    
   def add(key: String, v: Any) = {
     diceKey(key).foreach((mk)=> store.get(mk).getOrElse{store = store + ((mk, None))})
     store = store + ((key, Some(v)))
-    interested(key) foreach (send(_)(key, v))
+    interested(key, _.changeListeners) foreach (sendAdd(_)(key, v))
   }
 
-  val send = (to: StoreConnection)=>(x:(String, Any))=> x match {
+  val sendAdd = (to: StoreConnection)=>(x:(String, Any))=> x match {
     case (key, v) => {
     	println("sending to " + to.getID() + " " + key + " " + v)
     	to sendTCP (new ADD(key, v))
@@ -35,33 +42,71 @@ class Xushi {
     server.server.addListener(new Listener {
       override def connected(c: Connection) {
         val conn = c.asInstanceOf[StoreConnection]
-        peers = peers + ((conn, List()))
+        peers2 = conn :: peers2
       }
       override def disconnected(c: Connection) {
         val conn = c.asInstanceOf[StoreConnection]
-        peers = peers - conn
+        peers2 = peers2 - conn
       }
       
-      def removeFirst[A](num: A, list: List[A]) = list diff List(num)
+      def removeFirst[A](item: A, list: List[A]) = list diff List(item)
       
       override def received(c: Connection, obj: Object) {
         val conn = c.asInstanceOf[StoreConnection]
         obj match {
           case c: CMD => c match {
             case ADD(key, v) => add(key, v)
-            case LISTEN(key) => {
-              peers = peers + ((conn, key :: peers.get(conn).getOrElse(List())))
-              itemsForUpdate(key).foreach(send(conn))          
+            //case DEL(key)    => del(key)
+            case LISTEN(key,what) => {
+              println ("got listen " + key)
+              what match {
+                case x if x == "ADD" => 
+	              conn.changeListeners = key :: conn.changeListeners  
+	              itemsForUpdate(key).foreach(sendAdd(conn))
+                case x if x == "DEL" =>
+	              conn.delListeners = key :: conn.delListeners 
+              }
             }
-            case UNLISTEN(key)=>{
-              val v = peers.get(conn)
-              v.foreach{(x)=>peers = peers + ((conn, removeFirst(key, x)))}
+            case UNLISTEN(key,what)=>{
+              println ("got unlisten " + key)
+              what match {
+                case x if x == "ADD" => conn.changeListeners =removeFirst(key,conn.changeListeners)
+                case x if x == "DEL" => conn.delListeners = removeFirst(key,conn.changeListeners)
+              }
             }
+            
           }
           case _=>
         }
       }
     })
+  }
+}
+
+class StoreConnection extends Connection {
+  var changeListeners = List[String]()
+  var delListeners = List[String]()
+}
+
+class StoreServer(port: Int) extends Store(port) {
+  val server = new Server(16384, 2048, new KryoSerialization(kryi)) {
+    override def newConnection(): Connection = {
+      new StoreConnection
+    }
+  }
+
+  val accept = () => {
+    server.bind(port)
+    server.addListener(new Listener {
+      override def connected(c: Connection) {
+        println("new connection")
+      }
+      override def received(c: Connection, obj: Object) {
+        val conn = c.asInstanceOf[StoreConnection]
+      }
+    })
+
+    server.start()
   }
 }
 
